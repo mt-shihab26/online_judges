9b91796ea96936984d17dfd9f637da6ee314561c
Shihab Mahamud <shihab4t@gmail.com>
2022-06-20 01:13:21 +0600

    Python solutions

---
+++ Python/Finding_the_percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    total = 0
+    for ith in student_marks[query_name]:
+        total += ith
+    total /= 3
+    print("%.2f"%total)
