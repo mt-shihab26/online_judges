4549998d69b5f1eea55127c3a313b969f8872c7e
Shihab Mahamud <shihab4t@gmail.com>
2022-06-19 07:31:05 -0400

    1500 Rating problems

---
+++ 1500/1542B.Plus_and_Multiply.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long int ulli;
+#define endn "\n"
+
+//Solve
+void test(void) {
+    int n, a, b;
+    cin >>n >>a >>b;
+    int i = 0;
+    bool ok = false;
+    if (a == 1) {
+        if ((n-1) % b == 0) {
+            ok = true;
+        }
+    }
+    else {
+        lli pow_a = 1;
+        while (pow_a <= n) {
+            if ((n-pow_a) % b == 0) {
+                ok = true;
+                break;
+            }
+            pow_a *= a;
+        }
+    }
+    if (ok) {
+        cout <<"Yes";
+    }
+    else {
+        cout <<"No";
+    }
+    cout <<endn;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t;
+    cin >>t;
+    while (t--) {
+        test();
+    }
+
+    return 0;
+}
+// Solved By: shihab4t
+// Saturday, July 03, 2021 | 07:19:09 PM (+06)
---
+++ 1500/1675E.Replace_With_the_Previous_Minimize.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    int len = v.size();
+    for (int i = 0; i < len; i++)
+        cin >> v[i];
+    return istream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const set<T> &s)
+{
+    for (auto ith : s)
+        cout << ith << " ";
+    return ostream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const vector<T> &v)
+{
+    for (auto ith : v)
+        cout << ith << " ";
+    return ostream;
+}
+template <typename T, typename T2>
+ostream &operator<<(ostream &ostream, const pair<T, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T, typename T2>
+ostream &operator<<(ostream &ostream, const map<T, T2> &mp)
+{
+    for (auto ith : mp)
+        cout << ith << "\n";
+    return ostream;
+}
+
+#define str string
+#define vec vector
+#define endn '\n'
+#define test_cases \
+    int t;         \
+    cin >> t;      \
+    while (t--)    \
+        solve();
+
+/* -------------------- solution start -------------------- */
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    str s;
+    cin >> s;
+    int mx = 0;
+    char ch1 = 'z', ch2 = 'z';
+    for (int i = 0; i < n; i++)
+    {
+        if (mx >= s[i] - 'a')
+        {
+            s[i] = 'a';
+        }
+        else if (k > 0)
+        {
+            int diff = (s[i] - 'a') - mx;
+            if (k >= diff)
+            {
+                mx = max(mx, s[i] - 'a');
+                s[i] = 'a';
+                k -= diff;
+            }
+            else
+            {
+                ch1 = s[i];
+                s[i] -= k;
+                ch2 = s[i];
+                k = 0;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (ch2 <= s[i] && s[i] <= ch1)
+        {
+            s[i] = ch2;
+        }
+    }
+
+    cout << s << endn;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    test_cases;
+
+    return 0;
+}
+// Shihab Mahamud (github.com/shihab4t)
+// Friday, May 06, 2022 | 03:16:12 PM (+06)
