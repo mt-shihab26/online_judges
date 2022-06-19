9e786a28af8762810194485e1afd752f5a8dcd29
Shihab Mahamud <shihab4t@gmail.com>
2022-06-20 01:26:07 +0600

    solve 5 problems

---
+++ README.md
@@ -0,0 +1,4 @@
+# SpOJ
+[Sphere Online Judge](https://www.spoj.com) Problems Solution.
+
+My Profile: https://www.spoj.com/users/shihab4t
---
+++ TEST-Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Macros
+typedef long long int lli;
+typedef long double ld;
+#define endn "\n"
+// Functions
+// print vector all element in one line
+template <typename T> inline void print_vector(vector<T> &vac) {
+for (int ele : vac) cout << ele << " "; cout << endn; }
+
+
+// Solve
+void test(void) {
+    int num;
+    while (1) {
+        cin >> num;
+        if (num == 42)
+            break;
+        cout << num << endn;
+    }
+}
+
+int main(void) {
+    test();
+    return 0;
+}
+// shihab4t
+// Date: Monday, June 07, 2021 | 09:39:14 PM (+06)
