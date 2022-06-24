f7d1815839d4f7e0da3bf94ac1bd02bff2dd126c
Shihab Mahamud <shihab4t@gmail.com>
2022-06-25 00:23:15 +0600

    Solve 01

---
+++ 800/1694A.Creep.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+    int len = v.size();
+    for (int i = 0; i < len; i++)
+        cin >> v[i];
+    return istream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const set<T> &s)
+{
+    for (auto ith : s)
+        cout << ith << " ";
+    return ostream;
+}
+template <typename T>
+ostream &operator<<(ostream &ostream, const vector<T> &v)
+{
+    for (auto ith : v)
+        cout << ith << " ";
+    return ostream;
+}
+template <typename T, typename T2>
+ostream &operator<<(ostream &ostream, const pair<T, T2> &p)
+{
+    return (ostream << p.first << " " << p.second);
+}
+template <typename T, typename T2>
+ostream &operator<<(ostream &ostream, const map<T, T2> &mp)
+{
+    for (auto ith : mp)
+        cout << ith << "\n";
+    return ostream;
+}
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define str string
+#define vec vector
+#define all(x) x.begin(), x.end()
+
+bool solve()
+{
+    int zeros, ones;
+    cin >> zeros >> ones;
+
+    int mn = min(zeros, ones);
+    string st = "";
+
+    for (int i = 1; i <= mn; i++)
+    {
+        st += "01";
+        zeros--;
+        ones--;
+    }
+
+    while (zeros--)
+        st += '0';
+    while (ones--)
+        st += '1';
+
+    cout << st << ed;
+
+    return true;
+}
+
+bool test()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    test();
+
+    return 0;
+}
+// Shihab Mahamud (github.com/shihab4t)
+// Friday, June 24, 2022 | 07:17:02 PM (+06)
