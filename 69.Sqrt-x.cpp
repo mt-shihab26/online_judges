3c2a2e8710898518f66715a54c0da18379b0e1c4
Shihab Mahamud <shihab4t@gmail.com>
2022-06-27 17:57:59 +0600

    sqrt(n) binary search

--- 69.Sqrt-x.cpp
+++ 69.Sqrt-x.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int mySqrt(int x) {
-        if (x <= 1) return x;
+    int mySqrt(int x)
+    {
+        if (x <= 1)
+            return x;
         long long int low = 1, high = x, mid, ans;
-        while (low <= high) {
+        while (low <= high)
+        {
             mid = low + (high - low) / 2;
-            if ((mid * mid) == x) {
-                return mid;
-            }
-            if (mid * mid < x) {
+            if (mid * mid <= x)
+            {
                 ans = mid;
                 low = mid + 1;
             }
-            else {
+            else
+            {
                 high = mid - 1;
             }
-            cout << mid << " ";
         }
         return ans;
     }
 };
 
-int main() {
-    
+int main()
+{
 }
-
