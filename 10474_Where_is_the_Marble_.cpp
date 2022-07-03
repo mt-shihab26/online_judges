c0cdf1af30fd3d74e2cd5e446b734efdc5cd4fb4
Shihab Mahamud <shihab4t@gmail.com>
2022-07-03 16:14:23 +0600

    update solution

--- 10474_Where_is_the_Marble_.cpp
+++ 10474_Where_is_the_Marble_.cpp
@@ -1,71 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endn                '\n'
-#define umap                unordered_map
-#define uset                unordered_set
-#define pb                  push_back
+#define ed '\n'
+#define gp ' '
+#define lli long long int
 
-#define GLOBAL_ARRAY_SIZE   10000005
-#define LOCAL_ARRAY_SIZE    100005
-#define PI                  acos(-1.0)
-
-typedef short int               sint;
-typedef unsigned int            uint;
-typedef long long int           llint;
-typedef unsigned long long int  ullint;
-
-inline void first_io() {ios_base::sync_with_stdio(false);cin.tie(NULL);}
-inline void file_io() {freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);}
-vector<int> get_randoms(int amount, int first, int last) {vector<int> tmp;srand(time(0));for (int i = 0; i < amount; i++)tmp.push_back(first + ((rand() % (last - first + 1))));return tmp;}
-template <typename Tp> void print_array(Tp arr[], int n) {for (int i = 0; i < n; i++) cout << arr[i] << " "; cout << '\n';}
-template <typename Tp> void print_vector(const Tp &vc) {for (auto &ith : vc) cout << ith << " "; cout << '\n';}
-template <typename Tp> void print_stack(stack<Tp> st) {while (!st.empty()) { cout << st.top() << " "; st.pop();} cout << '\n';}
-
-
-int lower_bound(vector<int> arr, int tg) {
-    int low = 0, high = arr.size() - 1, mid, ans = -1;
-    while (low <= high) {
-        mid = low + (high - low) / 2;
-        if (arr[mid] >= tg) {
-            ans = mid;
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
+bool solve(int n, int q)
+{
+    vector<int> arr(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
     }
-    return ans;
+    sort(arr.begin() + 1, arr.end());
+    int tmp;
+    while (q--)
+    {
+        cin >> tmp;
+        auto it = lower_bound(arr.begin() + 1, arr.end(), tmp);
+        if (it != arr.end() && *it == tmp)
+            cout << tmp << " found at " << (it - arr.begin()) << ed;
+        else
+            cout << tmp << " not found" << ed;
+    }
+
+    return true;
 }
 
-int main(void) {
-    first_io();
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int n, m, tmp, i, i2 = 1, rs;
-    while (true) {
-        cin >> n >> m;
-        if (n == 0 && m == 0) return 0;
-        vector<int> vc;
-        for (i = 0; i < n; i++) {
-            cin >> tmp;
-            vc.push_back(tmp);
-        }
-        // print_vector(vc);
-        sort(vc.begin(), vc.end());
-        // print_vector(vc);
-
-        cout << "CASE# " << i2++ << ":" << endn;
-        while (m--) {
-            cin >> tmp;
-            rs = lower_bound(vc, tmp);
-            if (vc[rs] != tmp)
-                cout << tmp << " not found" << endn;
-            else 
-                cout << tmp << " found at " << rs+1 << endn;
-        }
+    int n, q, i = 1;
+    while (cin >> n >> q && n != 0 && q != 0)
+    {
+        cout << "CASE# " << i++ << ":\n";
+        solve(n, q);
     }
+
+    // solve();
 
     return 0;
 }
-// Solved by: Shihab Mahamud (github.com/shihab4t)
-// Thursday, October 07, 2021 | 06:12:33 AM (+06)
+// github.com/sh048b (Shihab Mahamud)
+// Sunday, July 03, 2022 | 02:50:55 PM (+06)
