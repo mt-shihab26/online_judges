348ba5d91c8abd1730c43c81d41b01d1f694b701
Shihab Mahamud <shihab4t@gmail.com>
2022-07-03 16:14:13 +0600

    Solve 01 Problems

--- 1088.Points_in_Segments.cpp
+++ 1088.Points_in_Segments.cpp
@@ -1,89 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define endn                '\n'
-#define umap                unordered_map
-#define uset                unordered_set
-#define pb                  push_back
+#define ed '\n'
+#define gp ' '
+#define lli long long int
 
-#define GLOBAL_ARRAY_SIZE   10000005
-#define LOCAL_ARRAY_SIZE    100005
-#define PI                  acos(-1.0)
-
-typedef short int               sint;
-typedef unsigned int            uint;
-typedef long long int           llint;
-typedef unsigned long long int  ullint;
-
-inline void first_io() {ios_base::sync_with_stdio(false);cin.tie(NULL);}
-inline void file_io() {freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);}
-vector<int> get_randoms(int amount, int first, int last) {vector<int> tmp;srand(time(0));for (int i = 0; i < amount; i++)tmp.push_back(first + ((rand() % (last - first + 1))));return tmp;}
-template <typename Tp> void print_array(Tp arr[], int n) {for (int i = 0; i < n; i++) cout << arr[i] << " "; cout << '\n';}
-template <typename Tp> void print_vector(const Tp &vc) {for (auto &ith : vc) cout << ith << " "; cout << '\n';}
-template <typename Tp> void print_stack(stack<Tp> st) {while (!st.empty()) { cout << st.top() << " "; st.pop();} cout << '\n';}
-
-
-template <typename Tp>
-int lower_bound(const vector<Tp> &arr, Tp x) {
-    int lw = 0, hi = arr.size()-1, ans = arr.size(), mid;
-    while (lw <= hi) {
+int lower_bound(const vector<int> &arr, int x)
+{
+    int lw = 0, hi = arr.size() - 1, ans = arr.size(), mid;
+    while (lw <= hi)
+    {
         mid = (hi + lw) / 2;
-        if (arr[mid] >= x) {
+        if (arr[mid] >= x)
+        {
             ans = mid;
-            hi = mid-1;
+            hi = mid - 1;
         }
-        else {
-            lw = mid+1;
-        }
+        else
+            lw = mid + 1;
     }
     return ans;
 }
 
-template <typename Tp>
-int upper_bound(const vector<Tp> &arr, Tp x) {
-    int lw = 0, hi = arr.size()-1, ans = arr.size(), mid;
-    while (lw <= hi) {
+int upper_bound(const vector<int> &arr, int x)
+{
+    int lw = 0, hi = arr.size() - 1, ans = arr.size(), mid;
+    while (lw <= hi)
+    {
         mid = (hi + lw) / 2;
-        if (arr[mid] > x) {
+        if (arr[mid] > x)
+        {
             ans = mid;
-            hi = mid-1;
+            hi = mid - 1;
         }
-        else {
-            lw = mid+1;
-        }
+        else
+            lw = mid + 1;
     }
     return ans;
 }
 
-void test_case() {
-    int n, q;
+bool solve()
+{
+    int n, q, first, second;
     cin >> n >> q;
-    vector<int> point(n);
-    for (int i = 0; i < n; i++) {
-        cin >> point[i];
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    while (q--)
+    {
+        cin >> first >> second;
+        // auto first_it = lower_bound(a.begin(), a.end(), first);
+        // auto second_it = upper_bound(a.begin(), a.end(), second);
+
+        auto first_it = lower_bound(a, first);
+        auto second_it = upper_bound(a, second);
+        cout << second_it - first_it << ed;
     }
-    int x, y;
-    while (q--) {
-        cin >> x >> y;
-        // auto up = upper_bound(point.begin(), point.end(), y);
-        // auto lw = lower_bound(point.begin(), point.end(), x);
-        int up = upper_bound(point, y);
-        int lw = lower_bound(point, x);
-        cout << up - lw << endn;
-    }
+
+    return true;
 }
 
-int main(void) {
-    first_io();
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int t, i = 1;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         cout << "Case " << i++ << ":\n";
-        test_case();
+        solve();
     }
+
+    // solve();
 
     return 0;
 }
-// Solved by: Shihab Mahamud (github.com/shihab4t)
-// Thursday, October 07, 2021 | 09:25:56 AM (+06)
+// github.com/sh048b (Shihab Mahamud)
+// Sunday, July 03, 2022 | 03:00:05 PM (+06)
--- README.md
+++ README.md
@@ -2,14 +2,4 @@
 
 [LightOJ](https://lightoj.com) Problems Solution.
 
-My Profile: https://lightoj.com/user/shihab4t
-
-<!-- ## KeyNotes
-Algorithms list:
-* [ ] [Brian Kernighan's Algorithm](https://sanchit3b.medium.com/brian-kernighans-algorithm-9e0ca5989148) -->
-
-<!-- 
-## Recursion
-
-## Co-odinate Geometry
-* https://lightoj.com/problem/bishops
+My Profile: https://lightoj.com/user/sh048b
