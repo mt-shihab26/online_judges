d41923759b521e0a6f7aaeb92341653c2096b476
Shihab Mahamud <shihab4t@gmail.com>
2022-07-17 04:30:49 +0600

    Solve 1 problems

---
+++ 0900/194A.Exams.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+
+bool solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    cout << (k <= n * 3 ? n * 3 - k : 0) << ed;
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Sunday, July 17, 2022 | 04:15:00 AM (+06)
