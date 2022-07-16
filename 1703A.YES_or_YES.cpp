01f2fb72d316b2d1475c86835035a59c3a2a2dd5
Shihab Mahamud <shihab4t@gmail.com>
2022-07-16 17:22:51 +0600

    Solve 2 problems

---
+++ 1703A.YES_or_YES.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+
+bool solve()
+{
+    string su;
+    cin >> su;
+
+    transform(su.begin(), su.end(), su.begin(), ::toupper);
+
+    cout << (su == "YES" ? "YES" : "NO") << ed;
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    // solve();
+
+    return 0;
+}
+// github.com/sh048b (Shihab Mahamud)
+// // Saturday, July 16, 2022 | 03:59:50 PM (+06)
---
+++ 1703B.ICPC_Balloons.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+
+bool solve()
+{
+    int n, ct = 0;
+    cin >> n;
+
+    bool a[95]; // a[65] = true;
+    for (int i = 0; i < 95; i++)
+        a[i] = false;
+
+    char c;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> c;
+        ct++;
+        if (a[c] == false)
+        {
+            ct++;
+            a[c] = true;
+        }
+    }
+
+    cout << ct << ed;
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    // solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Saturday, July 16, 2022 | 03:15:45 PM (+06)
