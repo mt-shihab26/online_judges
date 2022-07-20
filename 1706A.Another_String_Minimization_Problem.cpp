71d107af014f1600ee588c1f3a66fb2903594802
Shihab Mahamud <shihab4t@gmail.com>
2022-07-20 19:45:47 +0600

    Solve 2 problems

---
+++ 1706A.Another_String_Minimization_Problem.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+bool solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vec<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    vec<char> s(m + 1, 'B');
+
+    for (int i = 0; i < n; i++)
+    {
+        int ano = m + 1 - a[i];
+        int mn = min(ano, a[i]);
+        int mx = max(ano, a[i]);
+
+        if (s[mn] == 'B')
+            s[mn] = 'A';
+        else
+            s[mx] = 'A';
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        cout << s[i];
+    }
+    cout << ed;
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    // solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+//
---
+++ 1706B.Making_Towers.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+
+bool solve()
+{
+    int n;
+    cin >> n;
+
+    vec<int> a(n);
+    map<int, vec<int>> mp;
+    map<int, int> out;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mp[a[i]].push_back(i);
+        out[i + 1] = 0;
+    }
+
+    for (auto x : mp)
+    {
+        int len = x.second.size();
+        out[x.first] = 1;
+
+        bool even = false;
+
+        if (x.second[0] % 2 == 0)
+            even = true;
+
+        for (int i = 1; i < len; i++)
+        {
+            if (even && x.second[i] % 2 == 1)
+            {
+                out[x.first]++;
+                even = false;
+            }
+            else if (!even && x.second[i] % 2 == 0)
+            {
+                out[x.first]++;
+                even = true;
+            }
+        }
+    }
+
+    for (auto x : out)
+        cout << x.second << " ";
+
+    cout << ed;
+
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    // solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Wednesday, July 20, 2022 | 06:18:25 PM (+06)
