ccf3a6d72b4abd7c660a86545e170bc74a0cb230
Shihab Mahamud <shihab4t@gmail.com>
2022-07-22 04:27:55 +0600

    perfact

--- 0800/1527A.And_Then_There_Were_K.c
+++ 0800/1527A.And_Then_There_Were_K.c
@@ -3,21 +3,25 @@
 #include <string.h>
 #include <math.h>
 #include <ctype.h>
- 
-int main(void) {   
+
+int main(void)
+{
     int test, num;
     scanf("%d", &test);
-    while (test--) {
+    while (test--)
+    {
         scanf("%d", &num);
         int x = 1;
-        while (1) {
-            if (x*2 > num) {
-                printf("%d\n", x-1);
+        while (1)
+        {
+            if (x * 2 > num)
+            {
+                printf("%d\n", x - 1);
                 break;
             }
             x *= 2;
         }
     }
- 
+
     return 0;
 }
