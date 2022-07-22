22a2f70bcf8efb63d64546336d638d9401877da2
Shihab Mahamud <shihab4t@gmail.com>
2022-07-22 17:57:55 +0600

    Solve 3 problems

---
+++ 1200/1420B.Rock_and_Lever.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+bool solve()
+{
+    
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     solve();
+    // }
+
+    // solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Friday, July 22, 2022 | 05:43:49 PM (+06)
