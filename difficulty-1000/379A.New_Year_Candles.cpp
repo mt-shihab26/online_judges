3f2399637c7d90ff6c2710fc3b60bb3373a918b4
Shihab Mahamud <shihab4t@gmail.com>
2022-07-28 17:45:12 +0600

    Solve 2 1000 difficulty problems

---
+++ difficulty-1000/379A.New_Year_Candles.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+
+bool solve(int ti = 0)
+{
+    int a, b;
+    cin >> a >> b;
+
+    int hour = 0, candle = a, curr = 0;
+
+    while (candle)
+    {
+        candle--;
+        curr++;
+        hour++;
+        if (curr >= b)
+        {
+            candle++;
+            curr -= b;
+        }
+    }
+
+    cout << hour << ed;
+
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Thursday, July 28, 2022 | 05:39:35 PM (+06)
---
+++ difficulty-1000/500A.New_Year_Transportation.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define lli long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+
+bool solve(int ti = 0)
+{
+    int n, t;
+    cin >> n >> t;
+    vec<int> a(n + 1);
+    bool flag = false;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i <= n;)
+    {
+        if (i > t)
+            break;
+        if (a[i] + i == t)
+        {
+            flag = true;
+            break;
+        }
+        else
+            i += a[i];
+    }
+
+    cout << (flag ? "YES" : "NO") << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Thursday, July 28, 2022 | 05:00:16 PM (+06)
