9204a3e08a50368c1f31ea58d13d38c08358cba0
Shihab Mahamud <shihab4t@gmail.com>
2022-08-13 12:34:42 +0600

    Solve 2 1000 difficulty problems

---
+++ difficulty-0900/115A.Party.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, tmp, ct, ans = 0;
+    cin >> n;
+
+    vec<int> a(n + 1);
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    for (int i = 1; i <= n; i++)
+    {
+        tmp = a[i], ct = 0;
+        while (tmp >= 0)
+            tmp = a[tmp], ct++;
+        ans = max(ans, ct);
+    }
+
+    cout << ans + 1 << ed;
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Friday, August 12, 2022 | 07:25:48 PM (+06)
---
+++ difficulty-1000/1521A.Nastia_and_Nearly_Good_Numbers.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve(int ti = 0)
+{
+    ll a, b;
+    cin >> a >> b;
+
+    if (1 == b)
+    {
+        cout << "NO" << ed;
+        return 0;
+    }
+    cout << "YES" << ed;
+    cout << a * b << " " << a << " " << (a * b + a) << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, ti;
+    cin >> t;
+    for (ti = 0; ti < t; ti++)
+    {
+        solve(ti);
+    }
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Thursday, August 11, 2022 | 11:15:56 AM (+06)
