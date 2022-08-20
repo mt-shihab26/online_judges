852c27bea384cbca26f6ad31206c4b3dab28b4b1
Shihab Mahamud <shihab4t@gmail.com>
2022-08-21 04:49:17 +0600

    Solve 2 problems

---
+++ 1715A.Crossmarket.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int ans = (m - 1) + (n - 1) + m;
+    int ans2 = (n - 1) + (m - 1) + n;
+    if (n == 1 && m == 1)
+    {
+        ans = 0;
+    }
+    cout << min(ans, ans2) << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+//
