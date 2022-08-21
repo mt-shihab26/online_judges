01d7dd155cf88e0a898732617e94a7b1046c2fb7
Shihab Mahamud <shihab4t@gmail.com>
2022-08-21 17:09:02 +0600

    Solve 2 problems

---
+++ difficulty-1100/1176B.Merge_it.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    ll n, tmp;
+    cin >> n;
+    vec<ll> ctn(3);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        ctn[tmp % 3]++;
+    }
+
+    ll mn = min(ctn[1], ctn[2]);
+
+    ctn[1] = ctn[1] - mn;
+    ctn[2] = ctn[2] - mn;
+
+    ll ans = ctn[0] + mn + (ctn[1] / 3) + (ctn[2] / 3);
+
+    cerr << ctn[0] << gp << ctn[1] << gp << ctn[2] << ed;
+
+    cout << ans << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Sunday, August 21, 2022 | 03:29:31 PM (+06)
---
+++ difficulty-1500/550A.Two_Substrings.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    str s;
+    cin >> s;
+    int len = s.size();
+    pair<int, int> ab, ba;
+    ab = {-1, -1};
+    ba = {-1, -1};
+    for (int i = 1; i < len; i++)
+    {
+        if (s[i - 1] == 'A' && s[i] == 'B')
+        {
+            ab = {i - 1, i};
+            i++;
+        }
+        if (s[i - 1] == 'B' && s[i] == 'A')
+        {
+            ba = {i - 1, i};
+            i++;
+        }
+    }
+
+    str ans = ab.first >= 0 && ab.second >= 0 && ba.first >= 0 && ba.second >= 0 &&
+                      ab.second != ba.first && ab.first != ba.second
+                  ? "YES"
+                  : "NO";
+
+    if (ans == "NO")
+    {
+        ab = {-1, -1};
+        ba = {-1, -1};
+
+        for (int i = 1; i < len; i++)
+        {
+            if (s[i - 1] == 'B' && s[i] == 'A')
+            {
+                ba = {i - 1, i};
+                i++;
+            }
+            if (s[i - 1] == 'A' && s[i] == 'B')
+            {
+                ab = {i - 1, i};
+                i++;
+            }
+        }
+    }
+
+    // cerr << ab.first << ab.second << ed;
+    // cerr << ba.first << ba.second << ed;
+
+    ans = ab.first >= 0 && ab.second >= 0 && ba.first >= 0 && ba.second >= 0 &&
+                  ab.second != ba.first && ab.first != ba.second
+              ? "YES"
+              : "NO";
+
+    cout << ans << ed;
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Sunday, August 21, 2022 | 02:47:38 PM (+06)
