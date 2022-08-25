cac06d6bac93b7d2cd6d6fe586a98569b6fb23f0
Shihab Mahamud <shihab4t@gmail.com>
2022-08-25 06:06:51 +0600

    Solve 1 1200 difficulty problem

---
+++ difficulty-1100/363B.Fence.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    vec<int> a(n + 1, 0);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        a[i] = a[i - 1] + a[i];
+    }
+
+    int ans = INT_MAX, index = 0;
+    for (int i = 1, j = k; i <= n && j <= n; i++, j++)
+    {
+        int tmp = a[j] - a[i - 1];
+        if (ans > tmp)
+        {
+            index = i;
+            ans = min(ans, tmp);
+        }
+    }
+    cout << index << ed;
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Thursday, August 25, 2022 | 05:55:39 AM (+06)
