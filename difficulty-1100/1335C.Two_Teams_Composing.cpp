2c39e3e6cbc47413c18db153b3a684a5994426f2
Shihab Mahamud <shihab4t@gmail.com>
2022-08-29 14:14:46 +0600

    Solve 1 1100 difficulty problem

---
+++ difficulty-1100/1335C.Two_Teams_Composing.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n;
+    cin >> n;
+    vec<int> a(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+
+    set<int> as(a.begin(), a.end());
+
+    int mx_ele = mp.begin()->first, mx_size = mp.begin()->second;
+
+    for (auto ith : mp)
+    {
+        if (ith.second > mx_size)
+        {
+            mx_ele = ith.first;
+            mx_size = ith.second;
+        }
+    }
+
+    int first = as.size() - 1;
+    int second = mx_size;
+
+    if (second > first)
+    {
+        first++;
+        second--;
+    }
+
+    int ans = min(first, second);
+
+    cout << ans << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Sunday, August 28, 2022 | 05:55:44 PM (+06)
