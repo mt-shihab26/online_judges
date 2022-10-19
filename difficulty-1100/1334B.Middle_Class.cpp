4326995197c6147527594a8496a727afe51e335c
Shihab Mahamud <shihab4t@gmail.com>
2022-10-19 15:01:31 +0600

    Solved 2 1100 dificullty problems

---
+++ difficulty-1100/1334B.Middle_Class.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define int long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n, x;
+    cin >> n >> x;
+
+    vec<int> a(n);
+    int sm = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sm += a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    int i = n, j = 0;
+
+    while (j < n)
+    {
+        double tmp = 1.0 * sm / i;
+        if (tmp >= x)
+        {
+            cout << i << ed;
+            return true;
+        }
+        else
+        {
+            sm -= a[j];
+            i--;
+            j++;
+        }
+    }
+
+    cout << 0 << ed;
+
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Wednesday, October 19, 2022 | 06:12:04 AM (+06)
---
+++ difficulty-1100/1382B.Sequential_Nim.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+// 2 5 4
+// 1 -> 1
+// 1 -> 2
+// 4 -> 1
+// 1 -> 2
+// 4 -> 1
+
+// 1 -> 1
+// 1 -> 2
+// 1 -> 1
+// 1 -> 2
+// 1 -> 1
+// 1 -> 2
+// 1 -> 1
+// 1 -> 2
+
+// 6
+// 1 1 1 1 1 1 1 2
+
+bool solve()
+{
+    int n;
+    cin >> n;
+
+    vec<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    bool st = false;
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] == 1)
+            st = !st;
+        else
+            break;
+    }
+    if (i == n)
+    {
+        cout << (st ? "First" : "Second") << ed;
+        return true;
+    }
+    cout << (st ? "Second" : "First") << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Wednesday, October 19, 2022 | 02:12:20 PM (+06)
