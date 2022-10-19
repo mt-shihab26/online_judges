64d8b07b61248883c4c69297c8ffd2b2c1983509
Shihab Mahamud <shihab4t@gmail.com>
2022-10-20 02:12:06 +0600

    Solved 2 problems

---
+++ difficulty-1000/1469B.Red_and_Blue.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n;
+    cin >> n;
+    vec<int> a(n);
+
+    int mxa = 0, mxb = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (i != 0)
+            a[i] += a[i - 1];
+        mxa = max(mxa, a[i]);
+    }
+    int m;
+    cin >> m;
+    vec<int> b(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> b[i];
+        if (i != 0)
+            b[i] += b[i - 1];
+        mxb = max(mxb, b[i]);
+    }
+
+    // cerr << mxa << gp << mxb << ed;
+
+    cout << mxa + mxb << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Wednesday, October 19, 2022 | 08:32:20 PM (+06)
---
+++ difficulty-1100/1365A.Matrix_Game.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vec<vec<int>> a(n + 1, vec<int>(m + 1));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    vec<vec<int>> b(n + 1, vec<int>(m + 1));
+
+    int i, j, k;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= m; j++)
+        {
+            if (a[i][j] == 1)
+            {
+                for (k = 1; k <= m; k++)
+                    b[i][k] = 1;
+                for (k = 1; k <= n; k++)
+                    b[k][j] = 1;
+            }
+        }
+    }
+
+    bool st = false;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= m; j++)
+        {
+            if (b[i][j] == 0)
+            {
+                st = !st;
+                for (k = 1; k <= m; k++)
+                    b[i][k] = 1;
+                for (k = 1; k <= n; k++)
+                    b[k][j] = 1;
+            }
+        }
+    }
+    cout << (st ? "Ashish" : "Vivek") << ed;
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shihab4t (Shihab Mahamud)
+// Wednesday, October 19, 2022 | 09:49:44 PM (+06)
