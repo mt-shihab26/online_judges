2b9bb6cfcea440210b80fae6324e6391291b172c
Shihab Mahamud <shihab4t@gmail.com>
2022-11-05 10:26:31 +0600

    Solve 2 Problems

---
+++ difficulty-1100/1463A.Dungeon.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if ((a + b + c) % 9 != 0)
+        cout << "NO" << ed;
+    else if (min({a, b, c}) >= (a + b + c) / 9)
+        cout << "YES" << ed;
+    else
+        cout << "NO" << ed;
+
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shh26b (Shihab Mahamud)
+// Saturday, November 05, 2022 | 09:11:30 AM (+06)
---
+++ difficulty-1100/463B.Caisa_and_Pylons.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n;
+    cin >> n;
+    vec<int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int d = 0, e = 0;
+    int x = 0, y;
+
+    for (int i = 0; i < n; i++)
+    {
+        y = a[i];
+        e = x - y + e;
+
+        cerr << e << gp;
+
+        if (e < 0)
+        {
+            d += (-e);
+            e = 0;
+        }
+
+        x = a[i];
+    }
+    cerr << ed;
+
+    cout << d << ed;
+
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+// github.com/shh26b (Shihab Mahamud)
+// Thursday, November 03, 2022 | 12:27:08 PM (+06)
