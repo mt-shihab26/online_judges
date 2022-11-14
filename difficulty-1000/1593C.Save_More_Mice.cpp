e354cd5f7e8e38f7ae0321840062ba1da71c292e
Shihab Mahamud <shihab4t@gmail.com>
2022-11-14 21:08:05 +0600

    Solve 1 problems

---
+++ difficulty-1000/1593C.Save_More_Mice.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+bool solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vec<int> a(k);
+    for (int i = 0; i < k; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    int p = 0, ct = 0;
+    for (int i = 0; i < k; i++)
+    {
+        if (p < a[i])
+        {
+            p += (n - a[i]);
+            ct++;
+        }
+        else
+            break;
+    }
+    cout << ct << ed;
+
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shh26b (Shihab Mahamud)
+// Monday, November 14, 2022 | 01:00:23 PM (+06)
