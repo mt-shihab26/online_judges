d63d24b320ddb0f70e607086b20b9522497940cf
Shihab Mahamud <shihab4t@gmail.com>
2022-11-18 17:28:55 +0600

    Solved 1 problem of 1000 difficulty

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Handle: [`shihab4t`](https://codeforces.com/profile/shihab4t)
 
 <a href="https://codeforces.com/profile/shihab4t" target="_blank"><img src="https://img.shields.io/badge/dynamic/json?&color=1f8acb&logo=codeforces&label=Codeforces&url=https://competitive-coding-api.herokuapp.com/api/codeforces/shihab4t&query=%24.rating&prefix=Rating%20&style=for-the-badge&cacheSeconds=259200" alt="shihab4t's profile on Codeforces" title="shihab4t's profile on Codeforces"></a>
+
+## Links
+
+-   1000 - 1100: https://codeforces.com/problemset?order=BY_SOLVED_DESC&tags=1000-1100
---
+++ difficulty-1000/1506C.Double-ended_Strings.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ed '\n'
+#define gp ' '
+#define ll long long int
+#define vec vector
+#define str string
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const int inf = 1e9;
+
+vec<str> sub_strings(str s)
+{
+    vec<str> subs;
+    int n = s.length();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            str tmp = "";
+            for (int k = i; k <= j; k++)
+                tmp += s[k];
+            // cerr << tmp << ed;
+            subs.push_back(tmp);
+        }
+    }
+    // cerr << ed;
+    return subs;
+}
+
+bool solve()
+{
+    str a, b;
+    cin >> a >> b;
+
+    vec<str> subs_a = sub_strings(a);
+    vec<str> subs_b = sub_strings(b);
+    vec<str> ans;
+
+    int len_a = subs_a.size(), len_b = subs_b.size(), i, j;
+
+    for (i = 0; i < len_a; i++)
+        for (j = 0; j < len_b; j++)
+            if (subs_a[i] == subs_b[j])
+                ans.push_back(subs_a[i]);
+
+    int mx = INT_MIN, len;
+    for (i = 0; i < ans.size(); i++)
+    {
+        len = ans[i].length();
+        mx = max(mx, len);
+    }
+    int ct = (a.length() - mx) + (b.length() - mx);
+    cout << ct << ed;
+
+    return true;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
+// github.com/shh26b (Shihab Mahamud)
+// Friday, November 18, 2022 | 12:31:08 PM (+06)
