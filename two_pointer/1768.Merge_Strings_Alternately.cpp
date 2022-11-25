5b8769bfd4debf1fe7eb3c894c12c49e27ec72e9
Shihab Mahamud <shihab4t@gmail.com>
2022-11-26 05:01:02 +0600

    Reorder

--- two_pointer/1768.Merge_Strings_Alternately.cpp
+++ two_pointer/1768.Merge_Strings_Alternately.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    string mergeAlternately(string &word1, string &word2) {
+    string mergeAlternately(string &word1, string &word2)
+    {
         int i, j, len1 = word1.length(), len2 = word2.length();
         string ans = "";
-        for (i = 0, j = 0; i < len1 && j < len2; i++, j++) {
+        for (i = 0, j = 0; i < len1 && j < len2; i++, j++)
+        {
             ans += word1[i];
             ans += word2[j];
         }
-        while (i < len1) {
+        while (i < len1)
+        {
             ans += word1[i++];
         }
-        while (j < len2) {
+        while (j < len2)
+        {
             ans += word2[j++];
         }
         return ans;
     }
 };
 
-int main() {
-    
+int main()
+{
 }
