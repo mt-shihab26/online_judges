0595eda6400b2ed735f99c478d5d60cf4dcc086e
Shihab Mahamud <shihab4t@gmail.com>
2022-11-25 22:06:36 +0600

    Solve 1 problem

---
+++ pick_one/929.Unique_Email_Addresses.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    vector<string> split(string str, char spliter)
+    {
+        vector<string> ans;
+        string tmp = "";
+        for (char c : str)
+        {
+            if (c == spliter)
+            {
+                ans.push_back(tmp);
+                tmp = "";
+            }
+            else
+                tmp += c;
+        }
+        ans.push_back(tmp);
+        return ans;
+    }
+
+public:
+    int numUniqueEmails(vector<string> &emails)
+    {
+        set<string> st;
+        for (string email : emails)
+        {
+            vector<string> sp = split(email, '@');
+            string local_name = "";
+            for (char c : sp[0])
+            {
+                if (c == '+')
+                    break;
+                if (c != '.')
+                    local_name += c;
+            }
+            st.insert(local_name + '@' + sp[1]);
+        }
+        return st.size();
+    }
+};
+
+int main()
+{
+
+    return 0;
+};
+// github.com/shh26b (Shihab Mahamud)
+// Friday, November 25, 2022 | 10:05:22 PM (+06)
