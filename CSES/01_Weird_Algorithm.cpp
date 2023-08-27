8de516df4ec644a57388b8fe0b7ead11223cca12
Shihab Mahamud <shihab4t@gmail.com>
2023-08-28 04:56:05 +0600

    Updates

---
+++ CSES/01_Weird_Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long int n;
+    cin >> n;
+
+    cout << n << " ";
+
+    white (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+        else {
+            n *= 3 + 1
+        }
+
+        cout << n << " ";
+    }
+
+    cout << "\n";
+}
---
+++ dimikoj/C/71-লুকোচুরি-খেলা-ISCPC-2018-Preliminary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <ctype.h>
+// #include <stdlib.h>
+
+int main(void) {
+
+    // freopen("input.txt", "rt", stdin);
+    // freopen("output.txt", "wt", stdout);
+    
+    for (int i = 10; i > 0; i--) printf("%d\n", i);
+
+    return 0;
+}
+// Author: Shihab Mahamud
+// Date: Friday, April 09, 2021 | 08:26:57 AM
+// Problem Link: https://dimikoj.com/problems/71/iscpc-2018-preliminary-a
