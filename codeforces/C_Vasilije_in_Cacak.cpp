4322bde21cf608ec072d48263590bc155aa5a15b
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2023-11-03 11:24:13 +0600

    remove .clang-format to put it in the dotfiles repo

--- codeforces/C_Vasilije_in_Cacak.cpp
+++ codeforces/C_Vasilije_in_Cacak.cpp
@@ -8,30 +8,37 @@ using namespace std;
 #define str string
 #define xbe(x) x.begin(), x.end()
 
-template <typename T> void print(ostream &stream, const vec<T> &v) {
+template <typename T> void print(ostream &stream, const vec<T> &v)
+{
     for (const auto &e : v)
         stream << e << gp;
     stream << ed;
 }
 
-void solve() {
+void solve()
+{
     ll n, k, x;
     cin >> n >> k >> x;
 
     ll first_sum = ceil(k * 1.0 * (k + 1) / 2);
     ll last_sum = 0;
-    for (int i = 0, j = n; i < k; i++, j--) {
+    for (int i = 0, j = n; i < k; i++, j--)
+    {
         last_sum += j;
     }
     // cerr << n << gp << first_sum << gp << last_sum << ed;
-    if (first_sum <= x && x <= last_sum) {
+    if (first_sum <= x && x <= last_sum)
+    {
         cout << "YES" << ed;
-    } else {
+    }
+    else
+    {
         cout << "NO" << ed;
     }
 }
 
-int32_t main() {
+int32_t main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cerr << boolalpha;
